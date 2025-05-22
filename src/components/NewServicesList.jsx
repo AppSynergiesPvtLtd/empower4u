6641e319f256213e2cd7ccf5e3b51bf5989d6c93
sonal <sonalsingh4102@gmail.com>
2025-05-22 12:27:32 +0530

    url

--- src/components/NewServicesList.jsx
+++ src/components/NewServicesList.jsx
@@ -14,35 +14,35 @@ const services = [
 		desc:
 			"Our experienced team conducts thorough assessments to identify the causes of falls and develops personalized strategies to improve balance and walking stability. ",
 		img: "/images/home-page/tech.png",
-		url:"/specialty/spasticity-management"
+		url:"/specialty/falls-balance"
 	},
 	{
 		title: "Rare neurological conditions",
 		desc:
 			"We understand the unique challenges faced by individuals with rare neurological conditions such as neuromuscular diseases, Huntington’s disease, ataxia, ALS, cerebral palsy, and others. Our therapists use the latest approaches to deliver specialized care that meets your specific needs and goals.",
 		img: "/images/services/rare.avif",
-		url:"/specialty/spasticity-management"
+		url:"/specialty/rare-neurological-conditions"
 	},
 	{
 		title: "Other neurological conditions",
 		desc:
 			"Our dedicated team provides tailored therapy for individuals with conditions such as Parkinson's disease, multiple sclerosis, spinal cord injuries, and Guillain-Barre Syndrome. ",
 		img: "/images/services/other.avif",
-		url:"/specialty/spasticity-management"
+		url:"/specialty/other-neurological-conditions"
 	},
 	{
 		title: "Stroke Rehab",
 		desc:
 			"Our experienced therapists and assistants can support you to regain strength, mobility and functions.",
 		img: "/images/services/stroke.avif",
-		url:"/specialty/spasticity-management"
+		url:"/specialty/stroke-rehab"
 	},
 	{
 		title: "Vestibular Rehab",
 		desc:
 			"Our vestibular rehabilitation program is designed to help you regain your stability, confidence in walking, and ability to return to daily activities. ",
 		img: "/images/services/vestibular-rehab.avif",
-		url:"/specialty/spasticity-management"
+		url:"/specialty/vestibular-disorder"
 	},
 ];
 
